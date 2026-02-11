e11ece9a69f428bef16cd1a430a1522aa2cfb257
KONAKALLA KHASYAP SURYA SAKETH <118835857+khasyap@users.noreply.github.com>
2026-02-11 12:50:44 +0530

    Update Employee.sql

--- Employee.sql
+++ Employee.sql
@@ -1,4 +1,4 @@
-CREATE table Employee(EmployeeID int,EName varchar(50), EAge int, EGender varchar(6), ESalary int,EAddress varchar(50)); 
+CREATE table Employee(EmployeeID int NOT NULL UNIQUE AUTO_INCREMENT,EName varchar(50) NOT NULL, EAge int CHECK(EAge>=18), EGender varchar(6), ESalary int,EAddress varchar(50)); 
 
 insert into Employee values(01,"Surya",22,"M",25000,"Hyderabad"); 
 insert into Employee values(02,"Sree",22,"F",24000,"Guntur"); 
@@ -43,7 +43,22 @@ SELECT EMployeeID AS EID FROM Employee;
 SELECT EmployeeID FROM Employee GROUP BY EAddress;
 SELECT COUNT(ESalary) FROM Employee HAVING COUNT(EAge>22);
 SELECT EName FROM Employee WHERE EXISTS(SELECT EDepartment FROM Department WHERE Department.EmployeeID=Employee.EmployeeID);
+SELECT EmployeeID,EAge CASE WHEN EAge>24 THEN 'The Age is greather than 24' WHEN EAge=22 THEN 'The Age is equal to 22' ELSE 'The Age is under 22' END AS AgeText FROM Employee;
 
+ALTER TABLE Employee ADD Email varchar(50);
+ALTER TABLE Employee DROP Email;
+ALTER TABLE Employee RENAME  COLUMN EmployeeID to EmpID;
+ALTER TABLE Employee ALTER COLUMN EAge varchar(20);
+
+ALTER TABLE Employee DROP CHECK EAge;
+ALTER TABLE Employee ALTER JoinDate DEFAULT GETDATE();
+ALTER TABLE Employee ALTER JoinDate DROP DEFAULT;
+CREATE INDEX ENameID ON Employee(EmpID,EName);
+ALTER TABLE Employee DROP INDEX ENameID;
+UPDATE Employee SET Age=23 WHERE EName='Surya';
+GRANT SELCET UPDATE ON Employee TO user1;
+REVOKE SELECT ON Employee TO user1;
+UPDATE Employee SET Age=23 WHERE EName='Surya' COMMIT;
 -----------------------------------------------
 
 
@@ -51,12 +66,18 @@ select * from Employee;
 SELECT * INTO Employees FROM Employee -- it will crearte a copy of the table
 select DISTINCT(EAddress) from Employee;
 DELETE FROM Employee;
+ROLLBACK;
+SAVEPOINT Before_truncate;
+TRUNCATE TABLE Employee;
+
 DROP TABLE Employee;
 
+BACKUP DATABASE Employee TO DISK ='D:\backups\employee.bak';
+BACKUP DATABASE Employee TO DISK ='D:\backups\employee.bak' WITH DIFFERENTIAL; --A differential backup reduces the backup time  (since only the changes are backed up).
 
 
 
-CREATE table Department(EmployeeID int,, EDepartment varchar(20), ERole varchar(50), EExperience int,EAddress varchar(50));
+CREATE table Department(EmployeeID int PRIMARY KEY,DepartmentID int, EDepartment varchar(20), ERole varchar(50), EExperience int,EAddress varchar(50));
 
 
 
@@ -69,3 +90,8 @@ SELECT Employeee.EmployeeID,Departmet.ERole,Department.EJoining from Employee RI
 SELECT Employeee.EmployeeID,Departmet.ERole,Department.EJoining from Employee FULL JOIN Department ON Employee.EmployeeID=Department.EmployeeID;
 SELECT EAddress FROM Employee UNION SELECT EAddress FROM  Department;
 SELECT EAddress FROM Employee UNION all SELECT EAddress FROM  Department;
+
+
+
+CREATE TABLE Employees (EmpID int NOT NULL, DOJ int NOT NULL DepartmentID int, PRIMARY KEY(EmpID), FOREIGN KEY(DepartmentID) REFERENCES Department(DepartmentID);
+ALTER TABLE Employees DROP FOREIGN KEY DepartmentID;
